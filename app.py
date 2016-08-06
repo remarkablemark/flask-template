9da13060a115c664e1897e91710aa3733e1393b1
Mark <mark@remarkablemark.org>
2016-08-06 21:27:05 +0000

    Create and render a template for the index page

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 from flask import Flask
+from flask import render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    return render_template('index.html')
 
 app.run(
     debug=True,
